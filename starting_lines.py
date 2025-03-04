7feee189efc83891915c3206efbb93f6dc5a6659
GDENISS <mwangi.dennis21@students.dkut.ac.ke>
2025-03-04 14:31:47 +0300

    Day two practice

--- starting_lines.py
+++ starting_lines.py
@@ -1,3 +1,4 @@
+#Day one practice
 # conditional statements if else elif
 
 is_magician = False
@@ -9,3 +10,16 @@ elif is_magician and not is_expert:
     print('atleast you are getting there')
 elif not is_magician:
     print('you need magic powers')
+#conditional statements if else elif all are used to check the conditions and break the loop if the condition is met
+# if the condition is not met then the loop will continue to the next condition
+
+#Day two practice
+#adding various numbers
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+counter = 0
+for i in my_list:
+    counter += i
+print(counter)
+#the above is a simple example of adding numbers in a list using a for loop
+#the for loop will iterate through the list and add the numbers to the counter
