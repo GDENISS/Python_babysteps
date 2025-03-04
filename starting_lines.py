c1b5955a75b4d4ed32a0c4c4056bf2cf1975e171
GDENISS <mwangi.dennis21@students.dkut.ac.ke>
2025-03-04 07:48:25 +0300

    conditionals

---
+++ starting_lines.py
@@ -0,0 +1,11 @@
+# conditional statements if else elif
+
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print('you are a master magician')
+elif is_magician and not is_expert:
+    print('atleast you are getting there')
+elif not is_magician:
+    print('you need magic powers')
